59e4017bea34c6f86dbbd4437ebe16c50104476e
eiemon12 <ei.emon@yahoo.com>
2024-11-15 03:10:12 +0600

    setup project

---
+++ HMS/HMS/EF/HMSContext.cs
@@ -0,0 +1,14 @@
+﻿using HMS.EF.TableModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HMS.EF
+{
+    public class HMSContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ HMS/HMS/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace HMS.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HMS.EF.HMSContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(HMS.EF.HMSContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
